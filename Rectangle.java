5f4931cae54040dbd6b432dcd4a5c5adf7b2a7e4
Qurban Qurbanov <qurbanovq0612@gmail.com>
2026-03-06 08:57:37 +0400

    exercise 2 fully completed and tested with TestRectangle.java file

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,19 @@
-// testing if I did the right thing to submit my work
+public class Rectangle {
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Rectangle) {
+            Rectangle r = (Rectangle) obj;
+            if (this.width == r.width && this.height == r.height) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
